80d48829b324465c1ad37664df65247ba3a7a1b0
yasin <yasinmiapolash@gmail.com>
2024-12-02 00:13:03 +0600

    set json

--- lib/features/home/controllers/home_controller.dart
+++ lib/features/home/controllers/home_controller.dart
@@ -1,5 +1,9 @@
 import 'package:get/get.dart';
+import 'package:vpn/core/utils/constants/app_preferences.dart';
+import 'package:vpn/features/home/model/vpn_info_model_class.dart';
 
 class HomeController extends GetxController{
+final Rx<VpnInfo> vpnInfo=AppPreferences.vpnInfoObj.obs;
+
 
 }
